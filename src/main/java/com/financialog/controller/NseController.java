94159fc0a5713aa55d9bdab2cfac2e3f4cf69434
Naveen <79571862+NaveenJoyGit@users.noreply.github.com>
2022-09-29 23:21:50 +0530

    Implemented Aspect oriented logging to log execution time (#4)

--- src/main/java/com/financialog/controller/NseController.java
+++ src/main/java/com/financialog/controller/NseController.java
@@ -6,7 +6,6 @@ import com.financialog.service.NseDataService;
 import com.financialog.util.FinLogControllerPrefix;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
---
+++ src/main/java/com/financialog/util/LoggingAspect.java
@@ -0,0 +1,25 @@
+package com.financialog.util;
+
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Component
+public class LoggingAspect {
+
+    public static final Logger logger = LoggerFactory.getLogger(LoggingAspect.class);
+
+    @Around("@annotation(ExecutionLogging)")
+    public Object logExecutionTime(ProceedingJoinPoint joinPoint) throws Throwable {
+        long start = System.currentTimeMillis();
+        Object proceed = joinPoint.proceed();
+        long executionTime = System.currentTimeMillis() - start;
+        logger.info("*********************Execution Time of {} is {}", joinPoint.getSignature(), executionTime);
+        return proceed;
+    }
+
+}
